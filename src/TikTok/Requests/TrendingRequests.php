e761b13f61be047a25bcc579ee6731dfd806f5aa
Matt Grubb <mgrubb0507@gmail.com>
2020-11-02 17:35:05 -0500

    Supply download information for videos in responses

--- src/TikTok/Requests/TrendingRequests.php
+++ src/TikTok/Requests/TrendingRequests.php
@@ -31,6 +31,14 @@ class TrendingRequests
     // If there is an error, set the error in the parent, return false.
     if (isset($trending->error)) return $this->instance->setError($trending->message);
 
+    $trending->videoDownloadData = [
+      'userAgent' => $this->instance->endpoints->headers['m']['User-Agent'],
+      'cookies' => [
+        'tt_webid' => $this->instance->request->cookieJar->getCookieValue('tt_webid'),
+        'tt_webid_v2' => $this->instance->request->cookieJar->getCookieValue('tt_webid_v2')
+      ]
+    ];
+
     return $trending;
   }
 }
